f5879bb9ffd5a1686aeb2fb521b475e3c61af541
mercybassey <udohmercy911@gmail.com>
2024-02-29 16:57:06 -0500

    minor

--- src/initialize_clients.py
+++ src/initialize_clients.py
@@ -13,7 +13,7 @@ def initialize_clients():
    
     aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
     aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-    region_name = os.getenv('AWS_REGION', 'default-region')
+    region_name = os.getenv('AWS_REGION')
 
    
     s3_client = boto3.client(
